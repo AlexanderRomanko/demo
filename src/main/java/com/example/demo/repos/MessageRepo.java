3f444e446fbac7cf2faa8e18c4e9ec58fee146c1
AlexanderRomanko <alexander.romanko.job@gmail.com>
2020-07-21 20:05:27 +0300

    added application.properties changed main.html

--- src/main/java/com/example/demo/repos/MessageRepo.java
+++ src/main/java/com/example/demo/repos/MessageRepo.java
@@ -1,6 +1,6 @@
 package com.example.demo.repos;
 
-import com.example.demo.Message;
+import com.example.demo.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MessageRepo extends CrudRepository<Message, Integer> {
