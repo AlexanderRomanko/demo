cda8304f15553de4ca324e158187b12e69b0a608
AlexanderRomanko <alexander.romanko.job@gmail.com>
2020-09-02 16:49:47 +0300

    new

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -25,24 +25,24 @@ public class UserController {
         return "userList";
     }
 
-//    @GetMapping("{id}")
-//    public String userEditForm(@PathVariable (value = "id") long id, Model model) {
-//        if (!userRepository.existsById(id)) {
-//            return "redirect:/main";
-//        }
-//        Optional<User> user = userRepository.findById(id);
-//        ArrayList<User> res = new ArrayList<>();
-//        user.ifPresent(res::add);
-//        model.addAttribute("user", res);
-//        model.addAttribute("roles", Role.values());
-//        return "userEdit";
-//    }
-
-    @GetMapping("{user}")
-    public String userEditForm(@PathVariable User user, Model model) {
-        model.addAttribute("user", user);
+    @GetMapping("{id}")
+    public String userEditForm(@PathVariable (value = "id") long id, Model model) {
+        if (!userRepository.existsById(id)) {
+            return "redirect:/main";
+        }
+        Optional<User> user = userRepository.findById(id);
+        ArrayList<User> res = new ArrayList<>();
+        user.ifPresent(res::add);
+        model.addAttribute("users", res);
         model.addAttribute("roles", Role.values());
         return "userEdit";
     }
+
+//    @GetMapping("{user}")
+//    public String userEditForm(@PathVariable User user, Model model) {
+//        model.addAttribute("user", user);
+//        model.addAttribute("roles", Role.values());
+//        return "userEdit";
+//    }
 
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,11 +9,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserDetailsService {
-    private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
