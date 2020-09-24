7609bd8639a3c5df1a933c086da35a8228257475
AlexanderRomanko <alexander.romanko.job@gmail.com>
2020-09-24 20:36:11 +0300

    getting values from th:checked

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,12 +6,10 @@ import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
@@ -36,6 +34,30 @@ public class UserController {
         model.addAttribute("users", res);
         model.addAttribute("roles", Role.values());
         return "userEdit";
+    }
+
+    @PostMapping("{id}")
+    public String userSave(
+//            @PathVariable (value = "id") long id,
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam String userId
+//            @RequestParam("userId") User user,
+            ) {
+        User user = userRepository.findById(Long.valueOf(userId)).orElseThrow();
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key))
+                user.getRoles().add(Role.valueOf(key));
+        }
+
+        userRepository.save(user);
+
+        return "redirect:/user";
     }
 
 }
